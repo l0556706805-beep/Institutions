2ae24b2324c937e5439751dfc2969257958cb57c
l0556706805-beep <l0556706805@gmail.com>
2026-01-14 10:25:09 +0200

    Update front- admin

--- Institutions/DTOs/UpdateUserDto.cs
+++ Institutions/DTOs/UpdateUserDto.cs
@@ -3,9 +3,9 @@
     public class UpdateUserDto
     {
         public int InstitutionId { get; set; }
-        public string FullName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; } // אופציונלי
-        public string Role { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; } // אופציונלי
+        public string? Role { get; set; }
     }
 }
